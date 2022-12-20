1e517d0bfe8f2913822984f9fdb252a995fda5e2
Александр Матвеев <admatveev17@gmail.com>
2022-12-21 00:28:31 +0300

    Добавлены urls и view-функции

--- yatube/post/views.py
+++ yatube/post/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+# Главная страница
+def index(request):
+    return HttpResponse('Главная страница')
+
+
+def group_posts(request, slug):
+    return HttpResponse('Посты, отфлитрованные по группам') 
